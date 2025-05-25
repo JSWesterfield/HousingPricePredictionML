2581fc88d861e94750b528b011c93f396e23d877
Jake Westerfield <jecobwesterfield@gmail.com>
2025-05-24 20:33:53 -0400

    adds in print dataset.head to read dataset

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -4,3 +4,5 @@ import pandas as pd
 url = "https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv"
 dataset = pd.read_csv(url)
 
+# View the first few rows
+print(dataset.head())
